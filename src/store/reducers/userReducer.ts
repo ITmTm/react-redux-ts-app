7fb3550d742a592191d52c6bc87905a6c76c943f
Timur <Stafr95@mail.ru>
2024-05-20 21:27:51 +0500

    Importing types

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,35 +1,4 @@
-interface UserState {
-		users: any[];
-		loading: boolean;
-		error: null | string;
-}
-
-	// Создание элегантного варианта с перечислениями с enum
-enum UserActionType {
-		FETCH_USERS = 'FETCH_USERS',
-		FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
-		FETCH_USERS_ERROR = 'FETCH_USERS_ERROR'
-}
-
-	// Создание интерфейся для каждого actions
-interface FetchUsersAction {
-		type: UserActionType.FETCH_USERS;
-}
-
-interface FetchUsersSuccessAction {
-		type: UserActionType.FETCH_USERS_SUCCESS;
-		payload: any[];
-}
-
-
-interface FetchUsersErrorAction {
-		type: UserActionType.FETCH_USERS_ERROR;
-		payload: string;
-}
-
-
-	// Объединение 3 actions воедино
-type UserAction = FetchUsersAction | FetchUsersSuccessAction | FetchUsersErrorAction
+import { UserAction, UserActionType, UserState } from "../../types/todo";
 
 const initialState: UserState = {
 		users: [],
