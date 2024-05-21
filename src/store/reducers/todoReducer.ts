aab7e9f17aa60e49036f55e5ec439cd9a3edae49
Timur <Stafr95@mail.ru>
2024-05-21 17:39:36 +0500

    Creating a case for each action

---
+++ src/store/reducers/todoReducer.ts
@@ -0,0 +1,17 @@
+import { TodoAction, TodoActionTypes, TodoState } from "../../types/todo";
+
+
+export const todoReducer = (state: TodoState, action: TodoAction): TodoState => {
+		switch (action.type) {
+				case TodoActionTypes.FETCH_TODOS:
+						return {}
+				case TodoActionTypes.FETCH_TODOS_SUCCESS:
+						return {}
+				case TodoActionTypes.FETCH_TODOS_ERROR:
+						return {}
+				case TodoActionTypes.SET_TODO_PAGE:
+						return {}
+				default:
+						return state
+		}
+}
