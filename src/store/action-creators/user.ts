3cc93b18ceb44c99c2c1ca31397a7492d3cc5959
Timur <Stafr95@mail.ru>
2024-05-21 17:11:32 +0500

    Creating an actionCreator using axios

---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,21 @@
+import { UserAction, UserActionType } from "../../types/todo";
+import { Dispatch } from "redux";
+import axios from "axios";
+
+
+export const fetchUsers = () => {
+		return async (dispatch: Dispatch<UserAction>) => {
+				try {
+						dispatch({type: UserActionType.FETCH_USERS});
+						const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+						setTimeout(() => {
+								dispatch({type: UserActionType.FETCH_USERS_SUCCESS, payload: response.data})
+						}, 500)
+				} catch (e) {
+						dispatch({
+								type: UserActionType.FETCH_USERS_ERROR,
+								payload: 'Произошла ошибка при загрузке пользователей!'
+						})
+				}
+		}
+}
