bb9be07d1687364cb1170f91619e95af8b59708f
Timur <Stafr95@mail.ru>
2024-05-21 19:42:11 +0500

    Implementation of pagination

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,11 +4,12 @@ import { useActions } from "../hooks/useActions";
 
 const TodoList: React.FC = () => {
 		const {page, todos, loading, limit, error} = useTypedSelector(state => state.todo);
-		const {fetchTodos} = useActions();
+		const {fetchTodos, setTodoPage} = useActions();
+		const pages = [1, 2, 3, 4, 5];
 
 		useEffect(() => {
 				fetchTodos(page, limit)
-		}, []);
+		}, [page]);
 
 		if (loading) {
 				return <h1>Идет загрузка...</h1>
@@ -22,8 +23,19 @@ const TodoList: React.FC = () => {
 				<div style={{fontSize: "1.5rem"}}>
 						{todos.map(todo =>
 								<div key={todo.id}>{todo.id} - {todo.title}</div>
-
 						)}
+						<div style={{display: 'flex', marginTop: 20, columnGap: 5, cursor: "pointer"}}>
+								{pages.map(p =>
+										<div
+												onClick={() => setTodoPage(p)}
+												key={p}
+												style={{border: p === page ? '2px solid green' : '1px solid gray', padding: 10}}
+										>
+												{p}
+										</div>
+								)}
+						</div>
+
 				</div>
 		);
 };
