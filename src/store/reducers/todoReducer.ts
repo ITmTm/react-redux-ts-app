da20bdfbf1f166f2e0f8bf003d90409436318d38
Timur <Stafr95@mail.ru>
2024-05-21 17:44:01 +0500

    Completing the reducer

--- src/store/reducers/todoReducer.ts
+++ src/store/reducers/todoReducer.ts
@@ -9,16 +9,16 @@ const initialState: TodoState = {
 		limit: 10
 }
 
-export const todoReducer = (state: TodoState, action: TodoAction): TodoState => {
+export const todoReducer = (state = initialState, action: TodoAction): TodoState => {
 		switch (action.type) {
 				case TodoActionTypes.FETCH_TODOS:
-						return {}
+						return {...state, loading: true}
 				case TodoActionTypes.FETCH_TODOS_SUCCESS:
-						return {}
+						return {...state, loading: false, todos: action.payload}
 				case TodoActionTypes.FETCH_TODOS_ERROR:
-						return {}
+						return {...state, loading: false, error: action.payload}
 				case TodoActionTypes.SET_TODO_PAGE:
-						return {}
+						return {...state, page: action.payload}
 				default:
 						return state
 		}
