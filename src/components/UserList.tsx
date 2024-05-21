bba826d1a91878d39177b8e8525d1463b4342b73
Timur <Stafr95@mail.ru>
2024-05-21 17:13:30 +0500

    Importing and using the useActions hook

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
 
 const UserList: React.FC = () => {
 		const {users, loading, error} = useTypedSelector(state => state.user);
-		return (
-				<div>
+		const {fetchUsers} = useActions();
 
+		useEffect(() => {
+				fetchUsers()
+		}, []);
+
+		if (loading) {
+				return <h1>Идет загрузка...</h1>
+		}
+
+		if (error) {
+				return <h1>{error}</h1>
+		}
+
+		return (
+				<div style={{fontSize: "1.5rem"}}>
+						{users.map(user =>
+								<div key={user.id}>{user.name}</div>
+						)}
 				</div>
 		)
 }
