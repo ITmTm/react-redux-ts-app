2b152e19876ac6230c4ed0ad840301c1532ddd8f
Timur <Stafr95@mail.ru>
2024-05-21 19:12:38 +0500

    Line wrapping code

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,4 +2,7 @@ import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { rootReducer } from "./reducers";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+// @ts-ignore
+export const store = createStore(
+		rootReducer,
+		applyMiddleware(thunk));
