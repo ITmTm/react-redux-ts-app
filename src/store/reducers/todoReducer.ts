790700939004473dc297f190e2ac534e5a33cb25
Timur <Stafr95@mail.ru>
2024-05-21 17:41:21 +0500

    Creating a default state

--- src/store/reducers/todoReducer.ts
+++ src/store/reducers/todoReducer.ts
@@ -1,6 +1,14 @@
 import { TodoAction, TodoActionTypes, TodoState } from "../../types/todo";
 
 
+const initialState: TodoState = {
+		todos: [],
+		page: 1,
+		loading: false,
+		error: null,
+		limit: 10
+}
+
 export const todoReducer = (state: TodoState, action: TodoAction): TodoState => {
 		switch (action.type) {
 				case TodoActionTypes.FETCH_TODOS:
